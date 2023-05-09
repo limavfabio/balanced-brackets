cd5afe7023da657fc616810f1735b38956f73dd8
limavfabio <92121463+limavfabio@users.noreply.github.com>
2023-05-09 11:37:42 -0300

    first try mob programming

--- challenge.rb
+++ challenge.rb
@@ -1,6 +1,42 @@
 
 def balanced_brackets?(string)
   # your code here
+  stack = []
+  brackets = {
+"(" => ")",
+"[" => "]" ,
+"{" => "}" 
+  }
+
+  string.each_char do |char|
+    if(brackets.include?(char))
+      stack << char
+    else
+      case char
+      when ")"
+        if stack.last == "("
+          stack.pop
+          break
+        end
+      when "]"
+        if stack.last == "["
+          stack.pop
+          break
+        end
+      when "}"
+        if stack.last == "{"
+          stack.pop
+          break
+        end
+      return false
+
+
+      end
+    
+    end
+  end
+
+  return stack.empty?
 end
 
 puts balanced_brackets?('(hello)[world]')
